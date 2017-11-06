f8b4416429c3fb39c6879b569253968498a8ac46
student <mwoolery@ch112011m.nwmissouri.edu>
2017-11-06 13:57:56 -0600

    working on db manager

--- QuestionOfTheDay/DatabaseManager.swift
+++ QuestionOfTheDay/DatabaseManager.swift
@@ -39,4 +39,177 @@ class DatabaseManager {
     }
     
     
+    // returns all movies in the database
+    func retrieveAllOpinionsAsynchronously() {
+        let dispatchQueue = DispatchQueue(label: "clearer", qos:.userInteractive)
+        
+        dispatchQueue.async {
+            
+            let numOpinionToFetch = self.dataStoreOpinion?.getObjectCount() as! Int
+            let pageSize = 10
+            let queryBuilder = DataQueryBuilder()
+            var numOpinionFetched = 0
+            var allOpinions:[Opinion] = []
+            queryBuilder!.setPageSize(Int32(pageSize)).setOffset(0)
+            
+            while numOpinionFetched < numOpinionToFetch {
+                let opinion = self.dataStoreOpinion?.find(queryBuilder) as! [Opinion]
+                allOpinions += opinion
+                numOpinionFetched += opinion.count
+                queryBuilder!.prepareNextPage()
+            }
+            NotificationCenter.default.post(name:NSNotification.Name(rawValue:"All Opinions Fetched"), object:allOpinions)
+        }
+        
+    }
+    
+    // returns all movies in the database
+    func retrieveAllOpinions() ->[Opinion]{
+        
+        let numMoviesToFetch = dataStoreOpinion?.getObjectCount() as! Int
+        let pageSize = 10
+        let queryBuilder = DataQueryBuilder()
+        var numOpinionFetched = 0
+        var allOpinions:[Opinion] = []
+        queryBuilder!.setPageSize(Int32(pageSize)).setOffset(0)
+        
+        while numOpinionFetched < numMoviesToFetch {
+            let opinions = self.dataStoreOpinion?.find(queryBuilder) as! [Opinion]
+            allOpinions += opinions
+            numOpinionFetched += opinions.count
+            queryBuilder!.prepareNextPage()
+        }
+        return allOpinions
+    }
+    
+    
+    // retrns all opinions of a specified question
+    func retrieveOpinions(of question:QuestionOfTheDay) -> [Opinion] {
+        
+        let loadRelationsQueryBuilder = LoadRelationsQueryBuilder.of(QuestionOfTheDay.ofClass())
+        loadRelationsQueryBuilder!.setRelationName("opinions")
+        var allOpinions:[Opinion] = []
+        
+        while true {
+            // retrieve a page of relatedObjects (Movies)
+            let relatedObjects = (dataStoreQuestionOfTheDay?.loadRelations(question.objectId, queryBuilder: loadRelationsQueryBuilder))! as! [Opinion]
+            if relatedObjects.count == 0 { // no more Movies? we're out of here.
+                break
+            }
+            allOpinions += relatedObjects
+            
+            loadRelationsQueryBuilder!.prepareNextPage() // move on to the next page
+        }
+        
+        return allOpinions
+    }
+    
+    
+    // returns all directors
+    func retrieveAllQuestionOfTheDays() ->[QuestionOfTheDay]{
+        
+        let numQuestionOfTheDayToFetch = dataStoreQuestionOfTheDay?.getObjectCount() as! Int
+        let pageSize = 10
+        let queryBuilder = DataQueryBuilder()
+        var numQuestionOfTheDayFetched = 0
+        var allQuestionOfTheDay:[QuestionOfTheDay] = []
+        queryBuilder!.setPageSize(Int32(pageSize)).setOffset(0)
+        
+        while(numQuestionOfTheDayFetched < numQuestionOfTheDayToFetch) {
+            let questionOfTheDay = self.dataStoreQuestionOfTheDay?.find(queryBuilder) as! [QuestionOfTheDay]
+            allQuestionOfTheDay += questionOfTheDay
+            numQuestionOfTheDayFetched += questionOfTheDay.count
+            queryBuilder!.prepareNextPage()
+        }
+        return allQuestionOfTheDay
+    }
+    
+    func retrieveAllQuestionOfTheDaysAsynchronously() -> Void {
+        let dispatchQueue = DispatchQueue(label: "clearer", qos:.userInteractive)
+        
+        dispatchQueue.async {
+            let numQuestionOfTheDayToFetch = self.dataStoreQuestionOfTheDay?.getObjectCount() as! Int
+            let pageSize = 10
+            let queryBuilder = DataQueryBuilder()
+            var numQuestionOfTheDayFetched = 0
+            var allQuestionOfTheDay:[QuestionOfTheDay] = []
+            queryBuilder!.setPageSize(Int32(pageSize)).setOffset(0)
+            
+            while(numQuestionOfTheDayFetched < numQuestionOfTheDayToFetch) {
+                let questionOfTheDay = self.dataStoreQuestionOfTheDay?.find(queryBuilder) as! [QuestionOfTheDay]
+                allQuestionOfTheDay += questionOfTheDay
+                numQuestionOfTheDayFetched += questionOfTheDay.count
+                queryBuilder!.prepareNextPage()
+            }
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue:"All Directors Fetched"), object: allQuestionOfTheDay)
+        }
+    }
+    
+    // clears the entire database asynchronously
+    func clearDatabase(){
+        Types.tryblock(
+            { ()->Void in
+                let dispatchQueue = DispatchQueue(label: "clearer", qos:.utility)
+                dispatchQueue.async {  self.clearDatabaseSynchronously() }
+        },catchblock:{(fault)->Void in print("Problem clearing database: \(String(describing: fault))")}
+        )
+    }
+    
+    // clears the database
+    func clearDatabaseSynchronously(){
+        // clear movies first
+        let allMovies = self.retrieveAllMovies()
+        for movie in allMovies {
+            print("Removing \(movie)")
+            let dateStampRemoved = self.dataStoreMovie?.remove(movie)
+            print("date stamped removed: \(String(describing: dateStampRemoved))")
+        }
+        
+        let allDirectors = self.retrieveAllDirectors()
+        print(allDirectors)
+        for director in allDirectors {
+            print("Removing director \(director)")
+            let dateStampRemoved = self.dataStoreDirector?.remove(director)
+            print("date stamped removed: \(String(describing: dateStampRemoved))")
+        }
+    }
+    
+    // populates the entire database asynchronously
+    func populateDatabase(){
+        
+        let dispatchQueue = DispatchQueue(label: "clearer", qos:.utility)
+        dispatchQueue.async {
+            Types.tryblock(
+                { ()->Void in
+                    self.populateDatabaseSynchronously()
+            },
+                catchblock:{(fault)->Void in print("We had an owie: \(String(describing:fault))")}
+            )
+        }
+    }
+    
+    
+    // populates the entire database
+    func populateDatabaseSynchronously(){
+        
+        // save all the directors (non-blocking)
+        for (director, movies) in DatabaseManager.portfolio {
+            let savedDirector = dataStoreDirector?.save(director) as! Director // save a Director
+            var savedMovies:[String] = []
+            
+            for movie in movies {
+                let savedMovie = dataStoreMovie?.save(movie) as! Movie // save each of their movies
+                
+                savedMovies.append(savedMovie.objectId!) // gather the array of objectIds
+                let _ = dataStoreMovie?.setRelation("director:Director:1", // each movie has 1 director
+                    parentObjectId:savedMovie.objectId,
+                    childObjects:[savedDirector.objectId ?? ""])
+            }
+            
+            let _ = dataStoreDirector?.addRelation("movies:Movie:n",
+                                                   parentObjectId: savedDirector.objectId,
+                                                   childObjects: savedMovies)
+        }
+    }
+    
 }
